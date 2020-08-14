c2d14b192f962770f321b97f03e38b4badb14eb3
Navadeep Ganesh U <52508011+NavadeepGaneshU@users.noreply.github.com>
2020-08-14 22:48:17 +0530

    Update Asm_Blinky.asm

--- TI_MCU/Asm_Blinky.asm
+++ TI_MCU/Asm_Blinky.asm
@@ -6,20 +6,20 @@
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
-init:										                   ;Address lable-initialisation
+init:					     ;Address lable-initialisation
        bic.w	#0001h, &PM5CTL0             ;disables GPIO power on HIGH Z
-       bis.b    #01h,   &P1DIR             ; setting PIN1.0 as output(for LED)
+       bis.b    #01h,   &P1DIR               ; setting PIN1.0 as output(for LED)
 
 main:
 		   xor.b    #01h,   &P1OUT             ; Toggle PIN1.0
 		   mov.w    #0FFFFh, R4                ;puts big number in R4
 
 delay:
-       dec.w	R4                           ;decrement R4
+       dec.w	R4                         ;decrement R4
        jnz      delay                      ;conditional branch instruction
                                            ;repeat until R4=0
 
-		   jmp      main                       ;repeat forever
+		   jmp      main           ;repeat forever
 		   NOP
 
 ;-------------------------------------------------------------------------------
